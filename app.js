86b4fca69634a471fe09855b0d0d009f6ac61c2a
Chawsusumon <44886995+Chawsusumon@users.noreply.github.com>
2019-02-01 11:07:09 +0900

    Second

---
+++ app.js
@@ -0,0 +1,11 @@
+const result=document.getElementById('results');
+const ip1=document.getElementById('ip1');
+const ip2=document.getElementById('ip2');
+const calc=document.getElementById('calc');
+const rand=document.getElementById('rand');
+const clr=document.getElementById('clr');
+calc.addEventListener('click',function(){
+    console.log(ip1.value);
+    console.log(ip2.value);
+    
+});
